2a4407fcfc89a4c373784ac0f60ade5e830142d5
tiakon <1036615650@qq.com>
2017-08-09 00:41:00 +0800

    units文件更新

--- src/com/tiakon/utils/JDBCUtil.java
+++ src/com/tiakon/utils/JDBCUtil.java
@@ -8,53 +8,50 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class JDBCUtil {
-	private static Properties prop = new Properties();
-	private static ThreadLocal<Connection> tcl = new ThreadLocal<Connection>();
-	static {
-		try {
-			prop.load(JDBCUtil.class
-					.getResourceAsStream("/com/tiakon/conf/jdbc.properties"));
-			// 加载驱动
-			Class.forName(prop.getProperty("driverClassName"));
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+    private static Properties prop = new Properties();
 
-	public static Connection getConnection() {
-		Connection conn = tcl.get();
-		if (conn == null) {
-			try {
-				conn = DriverManager.getConnection(prop.getProperty("url"),
-						prop.getProperty("user"), prop.getProperty("password"));
-				tcl.set(conn);
-			} catch (SQLException e) {
-				throw new RuntimeException(e.getMessage());
-			}
-		}
-		return conn;
-	}
+    static {
+        try {
+            prop.load(JDBCUtil.class
+                    .getResourceAsStream("/com/tiakon/conf/jdbc.properties"));
+            // 加载驱动
+            Class.forName(prop.getProperty("driverClassName"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public static void close(ResultSet rs, PreparedStatement pstm,
-			Connection conn) {
-		if (rs != null)
-			try {
-				rs.close();
-			} catch (SQLException e) {
-				throw new RuntimeException(e);
-			}
-		if (pstm != null)
-			try {
-				pstm.close();
-			} catch (SQLException e) {
-				throw new RuntimeException(e);
-			}
-		if (conn != null)
-			try {
-				tcl.remove();
-				conn.close();
-			} catch (SQLException e) {
-				throw new RuntimeException(e);
-			}
-	}
+    public static Connection getConnection() {
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(prop.getProperty("url"),
+                    prop.getProperty("user"), prop.getProperty("password"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        return conn;
+    }
+
+    public static void close(ResultSet rs, PreparedStatement pstm,
+                             Connection conn) {
+        if (rs != null)
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        if (pstm != null)
+            try {
+                pstm.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        if (conn != null)
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+    }
 }
