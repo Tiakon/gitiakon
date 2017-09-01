e792309cc8509ebe88a98472ee7d08a6f43dfa3d
tiakon <1036615650@qq.com>
2017-09-01 12:06:38 +0800

    工具类更新

---
+++ src/com/tiakon/utils/DateUtil.java
@@ -0,0 +1,27 @@
+package com.tiakon.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+
+	public static String formatDate(Date date,String format){
+		String result="";
+		SimpleDateFormat sdf=new SimpleDateFormat(format);
+		if(date!=null){
+			result=sdf.format(date);
+		}
+		return result;
+	}
+	
+	
+	public static Date formatString(String str,String format) throws Exception{
+		if(StringUtil.isEmpty(str)){
+			return null;
+		}
+		SimpleDateFormat sdf=new SimpleDateFormat(format);
+		return sdf.parse(str);
+	}
+	
+	
+}
