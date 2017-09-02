8e35ecf75127933cb4a00f7cd49007084b772c47
tiakon <1036615650@qq.com>
2017-09-03 01:23:18 +0800

    工具类更新

--- src/com/tiakon/utils/DateUtil.java
+++ src/com/tiakon/utils/DateUtil.java
@@ -5,23 +5,24 @@ import java.util.Date;
 
 public class DateUtil {
 
-	public static String formatDate(Date date,String format){
-		String result="";
-		SimpleDateFormat sdf=new SimpleDateFormat(format);
-		if(date!=null){
-			result=sdf.format(date);
-		}
-		return result;
-	}
-	
-	
-	public static Date formatString(String str,String format) throws Exception{
-		if(StringUtil.isEmpty(str)){
-			return null;
-		}
-		SimpleDateFormat sdf=new SimpleDateFormat(format);
-		return sdf.parse(str);
-	}
-	
-	
+    public static String formatDate(Date date, String format) {
+        String result = "";
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        if (date != null) {
+            result = sdf.format(date);
+        }
+        return result;
+    }
+
+    public static Date formatString(String str, String format) throws Exception {
+        if (StringUtil.isEmpty(str)) {
+            return null;
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.parse(str);
+    }
+
+    public static String getCurrentDateStr() {
+        return new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+    }
 }
