60316150552157348ce9a55aabd22a214ae4d78f
tiakon <1036615650@qq.com>
2017-09-03 01:22:42 +0800

    servlet文件更新

--- src/com/tiakon/servlet/UserServlet.java
+++ src/com/tiakon/servlet/UserServlet.java
@@ -3,19 +3,27 @@ package com.tiakon.servlet;
 import com.tiakon.entity.User;
 import com.tiakon.service.UserService;
 import com.tiakon.service.impl.UserServiceImpl;
+import com.tiakon.utils.DateUtil;
+import com.tiakon.utils.PropertiesUtil;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by Hoictas on 2017/8/30.
  */
-@WebServlet(name = "UserServlet")
 public class UserServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
@@ -26,19 +34,21 @@ public class UserServlet extends HttpServlet {
         response.setCharacterEncoding("utf-8");
         System.out.println("**************UserServlet.java");
         String action = request.getParameter("action");
-        if ("show".equals(action)) {
-            showUser(request, response);
+        if ("presave".equals(action)) {
+            userPreSave(request, response);
+        } else if ("save".equals(action)) {
+            userSave(request, response);
         }
     }
 
-    protected void showUser(HttpServletRequest request, HttpServletResponse response) {
+    protected void userPreSave(HttpServletRequest request, HttpServletResponse response) {
         try {
             HttpSession session = request.getSession();
             UserService userService = new UserServiceImpl();
 
             if (session.getAttribute("currentUser") != null) {
-                User DBUser = userService.showUser((User) session.getAttribute("currentUser"));
-                request.setAttribute("DBUser", DBUser);
+                User DBUser = userService.userPreSave((User) session.getAttribute("currentUser"));
+                request.setAttribute("currentUser", DBUser);
                 request.setAttribute("mainPage", "/user/userShow.jsp");
                 request.getRequestDispatcher("/mainTemp.jsp").forward(request, response);
             } else {
@@ -49,5 +59,67 @@ public class UserServlet extends HttpServlet {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    protected void userSave(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            boolean imageChange = false;
+            boolean flag = false;
+            FileItemFactory fileItemFactory = new DiskFileItemFactory();
+            ServletFileUpload servletFileUpload = new ServletFileUpload(fileItemFactory);
+
+            List<FileItem> fileItems = servletFileUpload.parseRequest(request);
+            Iterator<FileItem> iterator = fileItems.iterator();
+
+            HttpSession session = request.getSession();
+
+            User sessionUser = (User) session.getAttribute("currentUser");
+
+            while (iterator.hasNext()) {
+                FileItem fileItem = iterator.next();
+                if (fileItem.isFormField()) {
+                    String fieldName = fileItem.getFieldName();
+                    if ("nickNameInput".equals(fieldName)) {
+                        sessionUser.setNickName(fileItem.getString("UTF-8"));
+                    }
+                    if ("moodTextArea".equals(fieldName)) {
+                        sessionUser.setMood(fileItem.getString("UTF-8"));
+                    }
+
+                } else if (!"".equals(fileItem.getName())) {
+                    String imageName = DateUtil.getCurrentDateStr();
+                    //设置图片名
+                    sessionUser.setImageName(imageName + "." + fileItem.getName().split("\\.")[1]);
+                    String imagePath = PropertiesUtil.getValue("imagePath");
+                    fileItem.write(new File(imagePath + sessionUser.getImageName()));
+                    imageChange = true;
+                }
+            }
+
+            if (!imageChange) {
+                sessionUser.setImageName(sessionUser.getImageName().replaceFirst(PropertiesUtil.getValue("userImage"), ""));
+            }
+
+            UserService userService = new UserServiceImpl();
+            flag = userService.userSave(sessionUser);
+
+            if (flag) {
+                //设置图片的绝对路径
+                sessionUser.setImageName(PropertiesUtil.getValue("userImage") + sessionUser.getImageName());
+                session.setAttribute("currentUser", sessionUser);
+                request.setAttribute("flag", "success");
+                request.setAttribute("message", "保存成功!");
+            } else {
+                request.setAttribute("flag", "failure");
+                request.setAttribute("message", "保存失败!");
+            }
+
+            request.setAttribute("mainPage", "/user/userShow.jsp");
+            request.getRequestDispatcher("/mainTemp.jsp").forward(request, response);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
